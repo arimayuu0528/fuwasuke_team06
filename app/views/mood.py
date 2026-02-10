353815e4f8fcffc16bb835896761336131105dbf
ecbo-dayo <mahiru109ra@gmail.com>
2026-02-10 14:32:04 +0900

    a
    aa

--- app/views/mood.py
+++ app/views/mood.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, url_for
 from app.db import DatabaseManager
 
 # Blueprintオブジェクト作成
@@ -8,14 +8,20 @@ mood_bp = Blueprint('mood',__name__,url_prefix='/mood')
 # -----------------------------------------------------
 # ○○画面処理　（エンドポイント：' ')  担当者名：
 # -----------------------------------------------------
-@mood_bp.route("/register_mood")
-def register_mood():
-    return render_template("register_mood.html")
+# @mood_bp.route("/register_mood")
+# def register_mood():
+#     return render_template("register_mood.html")
 
 
 
 
-@mood_bp.route("/home")
-def home():
-    return render_template("main/home.html")
+# @mood_bp.route("/home")
+# def home():
+#     return render_template("main/home.html")
+
+@mood_bp.route("/")
+def register():
+    url = url_for("register")
+    return render_template("register_mood.html", url)
+    
 
